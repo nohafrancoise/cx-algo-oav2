54b0d8efb023af7a041387b5501e533c52ca0ad1
Noha Francoise <noha.francoise@gmail.com>
2020-10-16 12:56:33 +0200

    .\e03bonus.js

---
+++ e03bonus.js
@@ -0,0 +1,17 @@
+const bubbletri = Array => {
+    let swap, i, p, end, taille = Array.length;
+    do {
+      swap = false;
+      for (let i = 0; i < Array.length; i++) {
+          for (p = 0, end = taille -i; p < end; p++) {
+        if (Array[p] < Array[p + 1]) {
+          let tmp = Array[p];
+          Array[p] = Array[p + 1];
+          Array[p + 1] = tmp;
+          swap = true;}
+        }
+      }
+    } while (swap);
+    return Array;
+  };
+  console.log(bubbletri([1, 7, 12, 567, 8, 90, 10, 0]))
